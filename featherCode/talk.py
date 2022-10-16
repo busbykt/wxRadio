7d24c1ded04796b6bcd4bee183c08bd4feb6a69a
busbykt <busbykt@mail.uc.edu>
2022-10-16 16:16:21 -0700

    update talk - add wind speed (but not to radio msg)

--- featherCode/talk.py
+++ featherCode/talk.py
@@ -4,6 +4,7 @@ import busio
 import digitalio
 import time
 import gc
+from analogio import AnalogIn
 # environmental sensor
 import adafruit_bme680
 # radio
@@ -21,19 +22,47 @@ reset = digitalio.DigitalInOut(board.RFM9X_RST)
 rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 915.0)
 rfm9x.tx_power=15
 
+# wind speed counter
+D11 = digitalio.DigitalInOut(board.D11)
+D11.switch_to_input(pull=digitalio.Pull.UP)
+
 while True:
 
     gc.collect()
     # Wait for confirmation from listener
     packet = rfm9x.receive(timeout=3.0)
+    
+    # get wind speed
+    curTime, count, lastValue = 0,0,0
+    window = 2
+    fixedTime = time.monotonic() # millisecond timer
+    while curTime < fixedTime+window:
+        # if switch closed and was previously open
+        if D11.value and not lastValue:
+            count+=1
+            lastValue=1
+            time.sleep(.01) # speed limit and "debounce"
+        # if reading the same value twice (when windspeed is low)
+        # do not iterate count
+        elif D11.value and lastValue:
+            pass
+        # wait 10ms and update lastValue
+        else:
+            lastValue=0
+            time.sleep(.01)
+        curTime = time.monotonic()
+        
+    # compute wind speed in mph
+    windSpeed = round(count*1.492 / window,1)
 
     # gather environmental data from sensor
-    temp = round(envSensor.temperature*9/5+32,2)
-    humidity = round(envSensor.humidity,2)
-    pressure = round(envSensor.pressure,2)
+    temp = round(envSensor.temperature*9/5+32,1)
+    humidity = round(envSensor.humidity,1)
+    pressure = round(envSensor.pressure,1)
 
     packetText = str(f'ZXT:{temp},H:{humidity},P:{pressure}QV', "ascii")
     print(packetText)
+    print(f'wind speed {windSpeed}')
 
     # If no packet was received during the timeout then None is returned.
     if packet is None:
@@ -42,13 +71,7 @@ while True:
         rfm9x.send(packetText)
 
     else:
-        # Received a packet!
-        # Print out the raw bytes of the packet:
-        # print("Received (raw bytes): {0}".format(packet))
-        # And decode to ASCII text and print it too.  Note that you always
-        # receive raw bytes and need to convert to a text format like ASCII
-        # if you intend to do string processing on your data.  Make sure the
-        # sending side is sending ASCII data before you try to decode!
+        # Received a packet
         print("Confirmation received.")
         print('Confirmation signal strength: {0} dB'.format(rfm9x.last_rssi))
 
